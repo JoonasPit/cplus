a2c62d37b1e056f23e9a9c8fbd99dae6f6f19231
Joonas <joonasmpitkaniemi@gmail.com>
2019-02-06 22:05:59 +0200

    Created

---
+++ 2/simplemaths.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int usernum;
+    int usernum_second;
+
+    cout << "Anna ensimmäinen luku: ";
+    cin >> usernum;
+    cout << "Anna toinen luku: ";
+    cin >> usernum_second;
+
+    cout << usernum + usernum_second <<endl;
+
+}
